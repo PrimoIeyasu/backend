9c3dee703c1027d9abfa62036d49000a6276b306
PrimoIeyasu <primoieyasu1@gmail.com>
2025-01-20 11:34:44 +0800

    123

--- your-project-name/config/database.php
+++ your-project-name/config/database.php
@@ -11,11 +11,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'gateway01.us-west-2.prod.aws.tidbcloud.com'),
+            'port' => env('DB_PORT', '4000'),
+            'database' => env('DB_DATABASE', 'enrollment'),
+            'username' => env('DB_USERNAME', 'RjMQMLXQ7RFbknU.root'),
+            'password' => env('DB_PASSWORD', 'zaeyrF32jZL5RSYR'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -24,7 +24,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', './certs/isrgrootx1.pem'),
             ]) : [],
         ],
 
